481e90b6edc72d9b078aa8986eb96b0d56b0cd2c
Amit Kumar <amit.jagga@outlook.com>
2026-07-06 13:56:00 -0700

    Initial commit - Azure Container Instance deployment via Terraform

---
+++ main.tf
@@ -0,0 +1,102 @@
+﻿
+terraform {
+
+  required_providers {
+
+    azurerm = {
+
+      source  = "hashicorp/azurerm"
+
+      version = "~> 4.0"
+
+    }
+
+  }
+
+
+
+  backend "azurerm" {
+
+    resource_group_name  = "rg-terraform-state"
+
+    storage_account_name = "tfstatelab2026"
+
+    container_name       = "tfstate"
+
+    key                  = "azure-container-infrastructure.tfstate"
+
+  }
+
+}
+
+
+
+provider "azurerm" {
+
+  features {}
+
+  subscription_id = "71b31346-1819-4f4d-9f6b-e873cfeeaf04"
+
+}
+
+
+
+resource "azurerm_resource_group" "main" {
+
+  name     = "rg-container-lab"
+
+  location = var.location
+
+}
+
+
+
+resource "azurerm_container_group" "web" {
+
+  name                = "aci-aj-nginx"
+
+  location            = azurerm_resource_group.main.location
+
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_address_type     = "Public"
+
+  os_type             = "Linux"
+
+
+
+  container {
+
+    name   = "aj-nginx"
+
+    image  = var.container_image
+
+    cpu    = var.cpu
+
+    memory = var.memory
+
+
+
+    ports {
+
+      port     = 80
+
+      protocol = "TCP"
+    }
+  }
+
+  tags = {
+    environment = "lab"
+    built-by    = "AJ Amit Kumar"
+  }
+}
+
+output "container_ip" {
+  description = "Public IP of the container"
+  value       = azurerm_container_group.web.ip_address
+}
+
+output "container_fqdn" {
+  description = "Fully qualified domain name"
+  value       = azurerm_container_group.web.fqdn
+}
---
+++ variables.tf
@@ -0,0 +1,47 @@
+﻿
+variable "location" {
+
+  description = "Azure region"
+
+  type        = string
+
+  default     = "East US"
+
+}
+
+
+
+variable "container_image" {
+
+  description = "Docker image to deploy"
+
+  type        = string
+
+  default     = "amit03773/aj-nginx:latest"
+
+}
+
+
+
+variable "cpu" {
+
+  description = "CPU cores for container"
+
+  type        = number
+
+  default     = 0.5
+
+}
+
+
+
+variable "memory" {
+
+  description = "Memory in GB for container"
+
+  type        = number
+
+  default     = 0.5
+
+}
+
